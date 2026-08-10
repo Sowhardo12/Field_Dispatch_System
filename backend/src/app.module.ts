29a150158e6951c02217c23bd6dce675695f3799
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2026-08-11 04:05:48 +0600

    for message queue phase, updated until AppModule.ts by adding the BullModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { PostgresModule } from './database/postgres/postgres.module';
 import { AppController } from './app.controller';
 import { AuthModule } from './modules/auth/auth.module';
 import { WorkOrdersModule } from './modules/work-orders/work-orders.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
@@ -12,14 +13,28 @@ import { WorkOrdersModule } from './modules/work-orders/work-orders.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+
     PostgresModule,
+
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
       }),
       inject: [ConfigService],
-    }),AuthModule,WorkOrdersModule,
+    }),
+    BullModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory:(configService:ConfigService)=>({
+        redis:{
+          host: configService.get<string>('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 5356),
+        },
+      }),inject:[ConfigService],
+    }),
+    AuthModule,
+    WorkOrdersModule,
+
   ],
   controllers: [AppController],
 })
